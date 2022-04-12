95402301eb2e195686a60c4b29127402f3b5ef88
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-04-12 23:58:57 +0300

    12.04.2022

--- src/Interview/Q3.java
+++ src/Interview/Q3.java
@@ -17,7 +17,35 @@ public class Q3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Lutfen bir cumle yaziniz");
-        String cumle="Java ile hersey guzel";
+        String cumle= scan.nextLine().toLowerCase();
 
+        hackerDil(cumle);
+    }
+    private static void hackerDil(String cumle) {
+        String arr[]=new  String[cumle.length()];
+
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i]=cumle.substring(i,i+1);
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        for (int i = 0; i <cumle.length() ; i++) {
+            if (arr[i].contains("s")){
+                arr[i] ="5";
+            }
+            if (arr[i].contains("a")){
+                arr[i] ="4";
+            }
+            if (arr[i].contains("e")){
+                arr[i] ="3";
+            }
+            if (arr[i].contains("i")){
+                arr[i] ="1";
+            }
+            if (arr[i].contains("o")){
+                arr[i] ="0";
+            }
+            System.out.print(arr[i]);
+        }
     }
 }
