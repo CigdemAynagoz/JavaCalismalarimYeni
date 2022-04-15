f194aabf93150c7938675579376cec5f519ae050
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-04-15 21:28:28 +0300

    15.04.2022

--- src/P09_kitapYonetimiTeam/K03_Main.java
+++ src/P09_kitapYonetimiTeam/K03_Main.java
@@ -1,11 +1,26 @@
 package P09_kitapYonetimiTeam;
 
-public class K03_Main {
-    /*
-Bu class main class olup tek satir olmalidir
- */
-        public static void main(String[] args) {
+import java.util.Scanner;
 
+import static P09_kitapYonetimiTeam.K01_Yonetimsel_Islemler.girisPaneli;
+import static P09_kitapYonetimiTeam.K01_Yonetimsel_Islemler.mevcutKitapListesi;
+import static P09_kitapYonetimiTeam.OgrenciIslemleri.ogrenciGirisPaneli;
+
+public class K03_Main {
+    public static void main(String[] args) {
+        System.out.println("*****JAVA KÜTÜPHANESİNE HOSGELDİNİZ*****");
+        System.out.print("Yonetici isleri icin 1\nOgrenci isleri icin 2\nSeciminizi yapiniz : ");
+        Scanner scan = new Scanner(System.in);
+        int secim = scan.nextInt();
+        if (secim == 1) {
+            mevcutKitapListesi();// mevcut kitap listesi kodu okumak kolay olsun diye koydum
+            girisPaneli();
+        } else if (secim == 2) {
+            mevcutKitapListesi();// mevcut kitap listesi kodu okumak kolay olsun diye koydum
+            ogrenciGirisPaneli();
+        }
 
     }
+
+
 }
--- src/P09_kitapYonetimiTeam/MusteriIslemleri.java
+++
@@ -1,9 +0,0 @@
-package P09_kitapYonetimiTeam;
-
-public class MusteriIslemleri {
-    public static void main(String[] args) {
-
-
-    }
-
-}
