0200063e60795b774dd3fe28d99d7fe2851f61c2
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-04-22 17:10:52 +0300

    22.04.2022

---
+++ src/HalukHocaProje/DepoProjesi/Main.java
@@ -0,0 +1,10 @@
+package HalukHocaProje.DepoProjesi;
+
+import static HalukHocaProje.DepoProjesi.Islemler.girisMenusu;
+
+public class Main {
+
+        public static void main(String[] args) {
+            girisMenusu();
+        }
+}
