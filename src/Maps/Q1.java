f4b637bf408f8d6c488cf5ab46f9d8bf56db6df5
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-04-23 23:30:34 +0300

    23.04.2022

--- src/Maps/Q1.java
+++ src/Maps/Q1.java
@@ -1,7 +1,18 @@
 package Maps;
 
+import java.util.HashMap;
+
 public class Q1 {
     //  Ad ve soyad bulunduruan iki ayrı array'i ad=soyad  seklinde yazdırınız.
 // input : {"ahmet", "ahmet can", "haluk"};  {"şeref", "erdem", "bilgin"};
 // output : {ahmet=şeref, ahmet can=erdem, haluk=bilgin};
+    public static void main(String[] args) {
+        String ad[]= {"ahmet", "ahmet can", "haluk"};
+        String soyAd[]= {"şeref", "erdem", "bilgin"};
+        HashMap<String,String> adSoyad=new HashMap<String,String>();
+        for (int i = 0; i <ad.length ; i++) {
+            adSoyad.put(ad[i],soyAd[i]);
+        }
+        System.out.println(" ad soyad : "+adSoyad);
+    }
 }
