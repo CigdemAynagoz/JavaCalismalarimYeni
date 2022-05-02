e79ab163632816628610271e7e30605e15641aad
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-05-02 10:43:28 +0300

    02.05.2022

--- src/Interview/Q1_Set.java
+++ src/Interview/Q1_Set.java
@@ -1,18 +1,21 @@
-package Set;
+package Interview;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Q3 {
+public class Q1_Set {
     //  PART 1
     // create a method that accepts ArrayList<Integer> and Set<Integer> as parameters
     // and add all elements from ArrayList to Set
+
     // ArrayList<Integer> ve Set<Integer> öğelerini parametre olarak kabul eden bir yöntem oluşturun
     // ve tüm öğeleri ArrayList'ten Set'e ekleyin
+
     // PART 2
     // create a method that takes Set<Character> and varargs of char as parameters
     // and adds all chars to the set
+
     // Set<Character> ve char değişkenlerini parametre olarak alan bir yöntem oluşturun
     // ve tüm karakterleri sete ekleyin
     public static void main(String[] args) {
@@ -23,25 +26,18 @@ public class Q3 {
         arrList.add(10);
         arrList.add(20);
         arrList.add(50);
-        System.out.println("arrList = " + arrList); //arrList = [10, 20, 30, 10, 20, 50]
+        System.out.println(arrList);//[10, 20, 30, 10, 20, 50]
 
         Set<Integer> set = new HashSet<>();
-        System.out.println("set = " + set); //set = []
-        arrayListdenSeteDonustur(arrList,set);
-        System.out.println("arrlisti sete attiktan sonra :" + set);
-        //Bolum2
-        Set<Character> characterSet = new HashSet<>();
-        karakterleriSeteEkle(characterSet,'A', 'B', 'R', 'r', 'q'  );
-        System.out.println("karakterler sete eklendikten sonra :" + characterSet);
+        System.out.println(set);//[]
+        arrListdenSeteDonustur(arrList,set);
+        System.out.println("Array listi ete attiktan sonra "+set);//[50, 20, 10, 30]
+
     }
-    private static void karakterleriSeteEkle(Set<Character> characterSet,Character ...chars) {
-        for (int i =0; i<chars.length; i++){
-            characterSet.add(chars[i]);
-        }
-    }
-    public static void arrayListdenSeteDonustur(ArrayList<Integer> arrList, Set<Integer> set) {
-        for(Integer sayi :arrList){
-            set.add(sayi);
+
+    private static void arrListdenSeteDonustur(ArrayList<Integer> arrList, Set<Integer> set) {
+        for (Integer each:arrList) {
+            set.add(each);
         }
     }
 }
---
+++ src/LambdaSoruCozumleri/Array_01.java
@@ -0,0 +1,23 @@
+package LambdaSoruCozumleri;
+
+import java.util.Scanner;
+
+public class Array_01 {
+    /* TASK :
+	 kullanicin girdigi sayi kadar int array olusturan ve array elemanlarini kullanicidan alan
+	 ve girilen array elemanlarinin en buyuk ile en kucuk elemaninin arasinki farki gosteren
+	 java code create ediniz.
+
+	 */
+    /*public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Kac elementli bir array istiyorsunuz");
+        int arr[]=new int[scan.nextInt()];
+        int uzunluk=scan.nextInt();
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println("Array'in "+(i+1)+".ci elementini giriniz");
+            uzunluk+=i;
+        }
+
+    }*/
+}
