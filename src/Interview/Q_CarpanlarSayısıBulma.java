746784637ba1e19d2aa2801d7a6a3955f00afdb7
Cigdem Aynagoz <c.ayngz.03@gmail.com>
2022-06-15 16:40:47 +0300

    15.06.2022

--- src/Interview/Q_CarpanlarSayısıBulma.java
+++ src/Interview/Q_CarpanlarSayısıBulma.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class Q_CarpanlarSayısıBulma {
     /*
+      Type java code by using while loop,
+      Write a program to count the factors of an integer which is entered by user.
+      Factors of 12 = 1, 2, 3, 4, 6, 12 ==> 6
+      Factors of 8  = 1, 2, 4, 8 ==> 4
+
     Java kodunu while döngüsünü kullanarak yazın, Kullanıcı tarafından girilen bir tamsayının
     çarpanlarını sayan bir program yazın.
     12'nin çarpanları = 1, 2, 3, 4, 6, 12 ==> 6
@@ -14,15 +19,16 @@ public class Q_CarpanlarSayısıBulma {
         System.out.println("Lütfen bir tamsayi giriniz");
         int sayi=Math.abs(scan.nextInt());
         int carpan=1;
-        int sayac=0;
+        int sayac = 0;
         while (carpan<=sayi){
             if (sayi%carpan==0){
-            sayac++;
-            System.out.println(carpan+" ");
+                sayac++;
+                System.out.print(carpan+" ");
             }
             carpan++;
         }
-
-        System.out.println(sayi+" sayısının "+sayac+" adet carpani vardir.");
+        System.out.println("");
+        System.out.println(sayi+" sayisinin "+ sayac+" adet carpani vardir");
     }
+
 }
---
+++ src/Set/interview.java
@@ -0,0 +1,36 @@
+package Set;
+
+import java.util.HashMap;
+
+public class interview {
+    //5 tane ulke ismi ve nufusunu oluşturuyoruz.(put)
+    //5 tane ulke ismi ve nufusunu yazdırıyoruz(country)
+    //5 tane ulke ismi yazdırıyoruz. (keyset())
+    //5 tane ulke nufusunu yazdırıyoruz. (values())
+    //nüfusların toplamını yazdırın.
+    //kaç tane 50 milyondan fazla nüfuslu ülke var?
+    // ulke ve nufus  lari hem map gibi..hem de ayri ayri yazdirma ?
+
+    public static void main(String[] args) {
+        HashMap<String,Integer> ulke = new HashMap<>();
+        ulke.put("Almanya", 80);
+        ulke.put("Turkiye", 83);
+        ulke.put("Amerika", 250);
+        ulke.put("Fransa", 68);
+        ulke.put("Kanada ", 40);
+        System.out.println("ulke isimleri ve nufuslari :" + ulke + " milyondur");
+
+        System.out.println("ulke isimleri : " + ulke.keySet());
+        //ulke isimleri : [Turkiye, Fransa, Almanya, Amerika, Kanada ]
+        System.out.println("ulke nufuslari :" + ulke.values());
+        //ulke nufuslari :[83, 68, 80, 250, 40]
+
+
+        System.out.println("nüfusu 50 milyondan fazla olan ülke sayisi :" + ulke.values().stream().filter(t->t>50).count());
+
+        ulke.entrySet().stream().forEach(t-> System.out.println(t.toString()));
+
+        System.out.println("ülkelerin nufuslari toplamı :" + ulke.values().stream().reduce(0,(x,y)->x+y));
+
+    }
+}
